047c686a56139108368cfaeb765a9756ba5d60a1
Jan Rudowski <j.rudowski@protonmail.com>
2022-09-14 19:45:32 +0200

    qwe

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TheCollapse />
+  <TheCollapse />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import TheCollapse from "./components/TheCollapse.vue";
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  margin: 0
+  padding: 0
+  display: flex
+  align-items: center
+  flex-direction: column
+  margin-top: 2rem
 </style>
---
+++ src/components/TheCollapse.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="TheCollapse">
+    <button @click="nOfItems++">add el</button>
+    <div class="header" @click="isMainOpen = !isMainOpen">Rozwiń mnie</div>
+    <div class="content" :class="{ active: isMainOpen }">
+      <div class="content__element">Lorem</div>
+      <div class="content__element">Ipsum</div>
+      <div class="content__element">Dolore</div>
+      <div class="content__element" @click="isInnerOpen = !isInnerOpen">
+        Rozwiń mnie
+      </div>
+
+      <div class="content__element__inner" :class="{ active: isInnerOpen }">
+        <div class="content__element__inner__item">qwe</div>
+        <div class="content__element__inner__item">asd</div>
+        <div class="content__element__inner__item">zxc</div>
+        <div
+          class="content__element__inner__item"
+          :key="n"
+          v-for="n in nOfItems"
+        >
+          item {{ n }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+const isMainOpen = ref(false);
+const isInnerOpen = ref(false);
+const nOfItems = ref(0);
+</script>
+<style lang="sass" scoped>
+.TheCollapse
+    display: flex
+    flex-direction: column
+    align-items: center
+    .content
+        color: blue
+        max-height: 0
+        overflow: hidden
+        background: gray
+        transition: max-height 500ms ease-in-out
+        &.active
+            max-height: 1000px
+        &__element
+            &__inner
+                max-height: 0
+                overflow: hidden
+                transition: max-height 500ms ease-in-out
+                &.active
+                    max-height: 1000px
+</style>
